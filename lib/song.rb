8667494d5be1dac737272db85243646219dcbd20
Robert Snyder <r.snyder49@gmail.com>
2018-08-27 20:27:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-    find(song_name) || create_by_name(song_name)
+    find_by_name(song_name) || create_by_name(song_name)
   end 
     
 
